0faa01305d3d2ea9fbf739e763e83beadd1a5002
dpbukin <d.bukin.dev@yandex.ru>
2023-11-23 21:22:57 +0300

    add Main file and simple code

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+        public static void main(String[] args) {
+            System.out.print("Это добавлено в ветке dev");
+
+            Scanner sc = new Scanner(System.in);
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+        }
+    }
