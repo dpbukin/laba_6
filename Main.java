4ecf735ce078280045d3c1420a77c4be97d51b7c
dpbukin <d.bukin.dev@yandex.ru>
2023-11-23 21:25:48 +0300

    edit Main class #1

--- Main.java
+++ Main.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 
 public class Main {
         public static void main(String[] args) {
-            System.out.print("Это добавлено в ветке dev");
+            System.out.println("Это добавлено в ветке dev");
 
             Scanner sc = new Scanner(System.in);
 
             int a = sc.nextInt();
             int b = sc.nextInt();
+
+            boolean soutBool = a < b ? true : false;
+
+            System.out.println(soutBool);
 
         }
     }
